4c5a89b8ea8ab6873a99a69e66c03b27f7449a94
Fedulov Anton, P <live.stone.anton@gmail.com>
2023-05-07 21:58:11 +0500

    feat: change port

--- config/server.ts
+++ config/server.ts
@@ -2,7 +2,7 @@ import { getEnv } from "./get-env";
 
 export default ({ env }) => ({
   host: getEnv(env, "HOST", "0.0.0.0"),
-  port: env.int("PORT", 1330),
+  port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
